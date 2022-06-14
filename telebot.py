f1dea51c5aac6ce08c171124417258e0dc067d59
BONUMMASTERPH211 <81369695+BONUMMASTERPH211@users.noreply.github.com>
2022-06-14 13:45:01 +0800

    Create telebot.py

---
+++ telebot.py
@@ -0,0 +1,28 @@
+import telegram.ext
+
+
+
+class Bot:
+	def __init__(self, token, update):
+		self.token = token
+		self.update = update
+
+	def start(self, update, context):
+		update.message.reply_text("Hi, code by f34rl3ss")
+
+
+def main():
+	print("> Bot is Running...")
+	token = "5531497971:AAFA6yzpCUpcpC_TomxWFTzqmA6FD6AP4Qg"
+	
+	updater = telegram.ext.Updater(token, use_context=True)
+	disp = updater.dispatcher
+
+	TG = (token, updater)
+	disp.add_handler(telegram.ext.CommandHandler("start", Bot.start))
+
+	updater.start_polling()
+	updater.idle()
+
+if __name__ == '__main__':
+	main()
